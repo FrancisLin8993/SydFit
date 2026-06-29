e3e6a35e2bcb1d6be2016483349c2a9a3f168c18
FrancisLin8993 <lfc1101@gmail.com>
2026-06-29 11:55:58 +0000

    put dist in gitignore

--- dist/openaiClient.js
+++
@@ -1,9 +0,0 @@
-import { withHeadroom } from "headroom-ai/openai";
-import OpenAI from "openai";
-import { loadConfig } from "./config.js";
-const config = loadConfig();
-export const openaiClient = withHeadroom(
-	new OpenAI({
-		apiKey: config.openaiApiKey,
-	}),
-);
--- dist/services/openaiClient.js
+++
@@ -1,7 +0,0 @@
-import { withHeadroom } from "headroom-ai/openai";
-import OpenAI from "openai";
-import { loadConfig } from "../utils/config.js";
-const config = loadConfig();
-export const openaiClient = withHeadroom(new OpenAI({
-    apiKey: config.openaiApiKey,
-}));
